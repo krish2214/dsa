994e979cb88deed3416054b4cd3970ce3a42f74c
krish2214 <seemasharma80970@gmail.com>
2025-10-16 22:06:16 +0530

    Add insertion sort in descending order
    Implement insertion sort algorithm to sort an array in descending order.

---
+++ c++ exercise/insertion_sort_in_descending_order.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int arr[]={5,3,1,4,2};
+    for(int i=1; i<n; i++){
+        int current = arr[i];
+        int j = i-1;
+        while(arr[j]<current && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}
